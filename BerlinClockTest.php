06bbfcf9ca4df8e70abfd51c6729a7bb2052a626
@thomas.lempereur <thomas.lempereur@student.vinci.be>
2020-10-27 14:58:10 +0100

    refactor - define berlinClock as an attribut

--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -6,21 +6,29 @@ use PHPUnit\Framework\TestCase;
 
 class BerlinClockTest extends TestCase
 {
+    private $berlinClock;
+
+    public function setUp(): void
+    {
+        $this->berlinClock = new BerlinClock();
+    }
+
     public function testTranslateToBerlinClockTimeGiven0minShouldReturnLightsOff() {
-        $berlinClock = new BerlinClock();
         $expectedTable = [""];
 
-        $actualTable = $berlinClock->translateToBerlinClockTime(0);
+        $actualTable = $this->berlinClock->translateToBerlinClockTime(0);
 
         $this->assertEquals($expectedTable, $actualTable);
     }
 
     public function testTranslateToBerlinClockGiven1minShouldReturnFirstLightOn(){
-        $berlinClock = new BerlinClock();
         $expectedTable = ["y"];
-        $actualTable = $berlinClock->translateToBerlinClockTime(1);
+
+        $actualTable = $this->berlinClock->translateToBerlinClockTime(1);
 
         $this->assertEquals($expectedTable, $actualTable);
     }
+
+
 
 }
